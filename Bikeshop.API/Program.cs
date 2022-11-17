91862f72fca8fbdea7e0a4faeb0ba3abdafb110b
Jan Schulze <58750565+ThatJanGuy@users.noreply.github.com>
2022-11-17 16:41:49 +0100

    Add AutoMapper as a service

--- Bikeshop.API/Program.cs
+++ Bikeshop.API/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<BikeshopContext>(
     dbContextOptions => dbContextOptions.UseSqlServer(
     builder.Configuration["ConnectionStrings:BikeshopDBConnectionString"]));
