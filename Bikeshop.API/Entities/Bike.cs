2efd4ed2e284a98fa06289b2fd7a9f30511855a3
Jan Schulze <58750565+ThatJanGuy@users.noreply.github.com>
2022-11-17 13:55:37 +0100

    Add entities Bike and Category

---
+++ Bikeshop.API/Entities/Bike.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZeBaikshobb.API.Entities
+{
+    public class Bike
+    {
+        public Bike(string name, string shortDescription, string fullDescription, double price, string pictureUrl)
+        {
+            Name = name;
+            ShortDescription = shortDescription;
+            FullDescription = fullDescription;
+            Price = price;
+            PictureUrl = pictureUrl;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string ShortDescription { get; set; }
+
+        [Required]
+        [MaxLength(10000)]
+        public string FullDescription { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public string PictureUrl { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+        public Guid CategoryId { get; set; }
+
+        public string? Colour { get; set; }
+    }
+}
---
+++ Bikeshop.API/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZeBaikshobb.API.Entities
+{
+    public class Category
+    {
+        public Category(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        public ICollection<Bike> Bikes { get; set; }
+            = new List<Bike>();
+    }
+}
